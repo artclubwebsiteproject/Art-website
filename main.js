f73873e3f2da7948acc9e68a983ab9e9eac7a128
Skyler Russell <77029203+skylerrussell@users.noreply.github.com>
2021-04-13 14:37:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,66 @@
-<alert>
-  
-<alert/>  
+<html>
+    <link rel="stylesheet" href="styles.css">
+<table border='1' bordercolor='Gold' cellspacing='0' cellpadding='0' align=center >
+    <tr><td><table color='white' cellspacing='0' cellpadding='0' align=center  border='0' height='500' width='1000'><td  align=center bgcolor='Gold'>  </td><td colspan=5 align=center bgcolor='Gold'><font size='3' face='Tahoma'color='white'>Apr 2021  </td><td  align=center bgcolor='Gold'> </td></tr><tr><td><font size='3' face='Tahoma'color='white'><b>Sun</b></font></td><td><font size='3' face='Tahoma'color='white'><b>Mon</b></font></td><td><font size='3' face='Tahoma'color='white'><b>Tue</b></font></td><td><font size='3' face='Tahoma'color='white'><b>Wed</b></font></td><td><font size='3' face='Tahoma'color='white'><b>Thu</b></font></td><td><font size='3' face='Tahoma'color='white'><b>Fri</b></font></td><td><font size='3' face='Tahoma'color='white'><b>Sat</b></font></td></tr><tr><td> </td><td> </td><td> </td><td> </td>
+    <td valign=top><font size='2' face='Tahoma'color='white'>1  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>2   
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>3  
+    <br>
+     </font></td></tr><tr><td valign=top><font size='h6' face='Tahoma'color='white'>4  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>5  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>6  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>7  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>8  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>9  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>10  
+    <br>
+     </font></td></tr><tr><td valign=top><font size='h6' face='Tahoma'color='white'>11  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>12  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>13  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>14  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>15  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>16  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>17  
+    <br>
+     </font></td></tr><tr><td valign=top><font size='h6' face='Tahoma'color='white'>18  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>19  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>20  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>21  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>22  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>23  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>24  
+    <br>
+     </font></td></tr><tr><td valign=top><font size='h6' face='Tahoma'color='white'>25  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>26  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>27  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>28  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>29  
+    <br>
+     </font></td><td valign=top><font size='h6' face='Tahoma'color='white'>30  
+    <br>
+     </font></td></tr></table></td></tr></table>
+</html>
